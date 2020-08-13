6816f9270c67b6b0e4c2a3fe197a39e33e3f84a1
harsh283 <harssh1999@gmail.com>
2020-08-13 13:21:35 +0530

    basic crud done

--- hcms-diagnostic_center-management-system/src/main/java/com/capg/hcms/diagnostic_center_management/controller/DiagnosticCenterController.java
+++ hcms-diagnostic_center-management-system/src/main/java/com/capg/hcms/diagnostic_center_management/controller/DiagnosticCenterController.java
@@ -1,16 +1,44 @@
 package com.capg.hcms.diagnostic_center_management.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capg.hcms.diagnostic_center_management.model.DiagnosticCenter;
+import com.capg.hcms.diagnostic_center_management.service.DiagnosticCenterServiceImplementation;
 
 
 @RestController
 public class DiagnosticCenterController {
+	@Autowired 
+	private DiagnosticCenterServiceImplementation centerService;
+	
+	@PostMapping("/addcenter")
+	public DiagnosticCenter addCenter(DiagnosticCenter center)
+	{
+		return centerService.addCenter(center);
+	}
+	@GetMapping("/getcenter/centerId/{centerId}")
+	public DiagnosticCenter getCenterById(@PathVariable String centerId)
+	{
+		return centerService.getCenterById(centerId);
+	}
+	@GetMapping("/getallcenters")
+	public List<DiagnosticCenter> getAllCenters()
+	{
+		return centerService.getAllCenters();
+	}
+	@DeleteMapping("/removecenter/centerId/{centerId}")
+	public boolean removeCenter(@PathVariable String centerId)
+	{
+		return centerService.removeCenter(centerId);
+	}
+	
 
 
 
--- hcms-diagnostic_center-management-system/src/main/java/com/capg/hcms/diagnostic_center_management/service/IDiagnosticCenterService.java
+++ hcms-diagnostic_center-management-system/src/main/java/com/capg/hcms/diagnostic_center_management/service/IDiagnosticCenterService.java
@@ -1,5 +1,13 @@
 package com.capg.hcms.diagnostic_center_management.service;
 
-public interface IDiagnosticCenterService {
+import java.util.List;
 
+import com.capg.hcms.diagnostic_center_management.model.DiagnosticCenter;
+
+public interface IDiagnosticCenterService {
+DiagnosticCenter addCenter(DiagnosticCenter center);
+List<DiagnosticCenter> getAllCenters();
+DiagnosticCenter getCenterById(String centerId);
+boolean removeAllCenters();
+boolean removeCenter(String centerId);
 }
